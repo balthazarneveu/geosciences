18fe52d7b470598445e8154cd69b1f7396394f2c
Balthazar Neveu <balthazarneveu@gmail.com>
2024-02-09 14:33:44 +0100

    #4 TP4 refactor a few functions

--- TP_4/visualizations.py
+++ TP_4/visualizations.py
@@ -2,6 +2,8 @@ from matplotlib import pyplot as plt
 import numpy as np
 import torch
 from constants import DEPTH_STEP
+import matplotlib.colors as mcolors
+from plane_cylinder_projections import image_vector_to_3d_plane_tangent
 
 
 def plot_ground_truth_3d(
@@ -54,7 +56,9 @@ def plot_ground_truth_3d(
     # ----------------- 3D quiver -----------------
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
-    colors = ['r', 'g', 'b', 'y']
+    # colors = ['r', 'g', 'b', 'y']
+    # colors = ["tab:orange", "tab:blue", "tab:green", "tab:red"]
+    colors = list(mcolors.TABLEAU_COLORS)
     # Extract x, y, z coordinates for plotting
     t3d = (p3D_gt[..., 1:, :] - p3D_gt[..., :-1, :])/3.
     for batch_index in range(p3D_gt.shape[0]):
@@ -79,3 +83,46 @@ def plot_ground_truth_3d(
     plt.legend()
     ax.set_title('Plane tangents in 3D space')
     plt.show()
+
+
+def validation_of_3d_tangent_estimation(
+    azimuth_coordinates_phi: torch.Tensor,
+    altitude_z: torch.Tensor,
+    p3D_gt: torch.Tensor = None,
+    batches_index: list = None
+):
+    """check that the jacobian computation of the 3D tangent
+    matches with the finite differences computation.
+    """
+    if batches_index is None:
+        batches_index = range(azimuth_coordinates_phi.shape[0])
+    for batch_idx in batches_index:
+        estimated_grad_list = []
+        if p3D_gt is not None:
+            tangent3d = (p3D_gt[..., 1:, :] - p3D_gt[..., :-1, :])
+        for element_idx in range(azimuth_coordinates_phi.shape[1]-1):
+            azi, alt = azimuth_coordinates_phi[batch_idx, element_idx], altitude_z[batch_idx, element_idx]
+            delta_azi = azimuth_coordinates_phi[batch_idx, element_idx+1] - \
+                azimuth_coordinates_phi[batch_idx, element_idx]
+            delta_alt = altitude_z[batch_idx, element_idx+1] - altitude_z[batch_idx, element_idx]
+            estimated_grad = image_vector_to_3d_plane_tangent(azi, alt, delta_azi, delta_alt)
+            estimated_grad_list.append(estimated_grad)
+        estimated_grad_list = torch.stack(estimated_grad_list)
+        for dim_idx, dim_name, dim_color in zip(range(3), "xyz", "rgb"):
+            plt.plot(
+                torch.rad2deg(azimuth_coordinates_phi[batch_idx, :-1]), estimated_grad_list[:, dim_idx],
+                color=dim_color, label=f"tangent {dim_name}"
+            )
+            if p3D_gt is not None:
+                plt.plot(
+                    torch.rad2deg(azimuth_coordinates_phi[batch_idx, :-1]), tangent3d[batch_idx, :, dim_idx],
+                    "--",
+                    color=dim_color,
+                    linewidth=4, alpha=0.5, label=f"tangent {dim_name} (gt)"
+                )
+        plt.xlabel("Azimuth (degrees)")
+        plt.ylabel("3D Tangent")
+        plt.legend()
+        plt.grid()
+        plt.title(f"Estimated 3D tangent by finite differences vs computing using the jacobian - slide {batch_idx}")
+        plt.show()
