b3cea9ba85db11131bb818c7babf4d70f57209fe
Balthazar Neveu <balthazarneveu@gmail.com>
2024-02-18 22:28:03 +0100

    #5 TP-5 remote training

--- TP_5/configuration.py
+++ TP_5/configuration.py
@@ -2,5 +2,6 @@ NB_ID = "training-notebook-geosciences"  # This will be the name which appears o
 GIT_USER = "balthazarneveu"  # Your git user name
 GIT_REPO = "geosciences"  # Your current git repo
 # Keep free unless you need to acess kaggle datasets. You'll need to modify the remote_training_template.ipynb.
-KAGGLE_DATASET_LIST = []
+KAGGLE_DATASET_LIST = ["balthazarneveu/mva-geosciences-segmentation-dataset-slb"]
 WANDBSPACE = "geosciences-segmentation"
+TRAIN_SCRIPT = "TP_5/train.py"  # Location of the training script
