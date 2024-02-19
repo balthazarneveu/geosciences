d6de9188c273042cdb85b20f388fd9a808e5bbe4
Balthazar Neveu <balthazarneveu@gmail.com>
2024-02-19 15:40:20 +0100

    #5 TP-5 segmentation export

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 # jupyter nbconvert TP_2/report/report.ipynb --to slides
 # python3 compile/edit_html.py TP_2/report/report.slides.html
-jupyter nbconvert TP_4/planes_synthesis.ipynb --to html
+# jupyter nbconvert TP_4/planes_synthesis.ipynb --to html
+jupyter nbconvert TP_5/segmentation.ipynb --to html
