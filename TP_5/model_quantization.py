31c1e644c086e89dfbd6c3ee584fce855132025e
Balthazar Neveu <balthazarneveu@gmail.com>
2024-03-04 22:53:52 +0100

    #7 updat quantization figures and report

--- TP_5/model_quantization.py
+++ TP_5/model_quantization.py
@@ -19,6 +19,7 @@ def load_model(exp: int, device: str = DEVICE):
 
 def main_quantization(exp: int = 201, device=DEVICE, num_bits: int = 16):
     model, _ = load_model(exp, device=device)
+    print(f"model {exp} has  {model.count_parameters()} parameters")
     quantized_weights, quantized_params = quantize_model_per_layer(model, num_bits=num_bits)
     return quantized_weights, quantized_params
 
--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 # jupyter nbconvert TP_2/report/report.ipynb --to slides
 # python3 compile/edit_html.py TP_2/report/report.slides.html
 # jupyter nbconvert TP_4/planes_synthesis.ipynb --to html
-# jupyter nbconvert TP_5/segmentation.ipynb --to html
-jupyter nbconvert TP_6/PINN.ipynb --to html
+jupyter nbconvert TP_5/segmentation.ipynb --to html
+jupyter nbconvert TP_5/model_quantization.ipynb --to html
+# jupyter nbconvert TP_6/PINN.ipynb --to html
 # jupyter nbconvert TP_3/TP_3.ipynb --to html
