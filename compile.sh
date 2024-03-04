6c6538ab66d1a559298021b9e2688608e5459d21
Balthazar Neveu <balthazarneveu@gmail.com>
2024-03-04 16:05:43 +0100

    #3 TP-3 add preliminary work - unfinished lab session

--- compile.sh
+++ compile.sh
@@ -2,4 +2,5 @@
 # python3 compile/edit_html.py TP_2/report/report.slides.html
 # jupyter nbconvert TP_4/planes_synthesis.ipynb --to html
 # jupyter nbconvert TP_5/segmentation.ipynb --to html
-jupyter nbconvert TP_6/PINN.ipynb --to html
+# jupyter nbconvert TP_6/PINN.ipynb --to html
+jupyter nbconvert TP_3/TP_3.ipynb --to html
