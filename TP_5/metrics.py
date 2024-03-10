2c5d8e25cd9ca2e0d92a87a5d9f1884d3a310b61
Balthazar Neveu <balthazarneveu@gmail.com>
2024-03-10 23:32:37 +0100

    #7 fix dice metric when there are no positive labels!

--- TP_5/metrics.py
+++ TP_5/metrics.py
@@ -26,13 +26,23 @@ def compute_metrics(y_pred: torch.Tensor, y_true: torch.Tensor, threshold: float
         - Intersection over Union (IoU)
     """
     reduction_dimension = (-1, -2, -3)
+
+    # total_shape = 1
+    # for idx in reduction_dimension:
+    #     total_shape *= y_pred.shape[idx]
+
     # Convert predictions to binary (0 or 1) based on the threshold
     y_pred_bin = (torch.sigmoid(y_pred) > threshold).float()
     # True Positives, False Positives, True Negatives, False Negatives
+    total_positives = (y_true == 1).float().sum(dim=reduction_dimension)
     true_positive = ((y_pred_bin == 1) & (y_true == 1)).float().sum(dim=reduction_dimension)
+    # WARNING! If there are no positives, we set the true positive to 0.1 to fake a single pixel 
+    true_positive = true_positive * (total_positives != 0) + (total_positives == 0) * \
+        torch.ones_like(true_positive) * 1.
     false_positive = ((y_pred_bin == 1) & (y_true == 0)).float().sum(dim=reduction_dimension)
     true_negative = ((y_pred_bin == 0) & (y_true == 0)).float().sum(dim=reduction_dimension)
     false_negative = ((y_pred_bin == 0) & (y_true == 1)).float().sum(dim=reduction_dimension)
+    # print(f"{true_positive.item()=:}, {false_positive.item()=:}, {true_negative.item()=:}, {false_negative.item()=:}")
 
     # Precision
     precision = true_positive / (true_positive + false_positive + epsilon)
