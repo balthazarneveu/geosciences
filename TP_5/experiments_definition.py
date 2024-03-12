f55db8cb1e2287bb618c3c17bc00b5a59018783a
Balthazar Neveu <balthazarneveu@gmail.com>
2024-03-12 18:46:27 +0100

    #7 intialize experiments

--- TP_5/experiments_definition.py
+++ TP_5/experiments_definition.py
@@ -20,31 +20,35 @@ from experiments_default import (
 
 def get_experiment_config_latest(exp: int) -> dict:
     config = default_experiment(exp)
-    if exp == 1:
-        experiment_stacked_convolutions(config, num_layers=5, h_dim=256, n=50)
-        config[DATALOADER][AUGMENTATION_LIST] = [AUGMENTATION_H_ROLL_WRAPPED, AUGMENTATION_FLIP]
-        config[LOSS] = LOSS_DICE_BCE
-    elif exp == 2:
-        vanilla_experiment(config, n=50, b=128)
-        config[LOSS] = LOSS_DICE_BCE
-        vanilla_experiment(config, )
-    elif exp == 3:  # Classic UNet
+    config[SCHEDULER_CONFIGURATION] = {
+        "factor": 0.8,
+        "patience": 10
+    }
+    config[DATALOADER][AUGMENTATION_LIST] = [AUGMENTATION_H_ROLL_WRAPPED, AUGMENTATION_FLIP]
+    config[OPTIMIZER][PARAMS][LR] = 1e-3
+    config[NB_EPOCHS] = 100
+    config[LOSS] = LOSS_DICE_BCE
+    if exp == 10:  # Vanilla experiment
+        vanilla_experiment(config, n=100, b=128)
+    elif exp == 20:  # MicoConv experiment
+        experiment_micro_conv(config, n=100, b=128, h_dim=4)
+    elif exp == 30:  # Stacked convolutions
+        experiment_stacked_convolutions(config, num_layers=5, h_dim=128, n=100)
+    elif exp == 40:  # Classic UNet
         config[LOSS] = LOSS_DICE_BCE
         config[DATALOADER][BATCH_SIZE][TRAIN] = 128
         config[DATALOADER][BATCH_SIZE][VALIDATION] = 128
         config[SCHEDULER] = REDUCELRONPLATEAU
-        config[SCHEDULER_CONFIGURATION] = {
-            "factor": 0.8,
-            "patience": 5
-        }
         config[MODEL][ARCHITECTURE]["channels_extension"] = 64
-        config[DATALOADER][AUGMENTATION_LIST] = [AUGMENTATION_H_ROLL_WRAPPED, AUGMENTATION_FLIP]
-        config[OPTIMIZER][PARAMS][LR] = 1e-4
-    if exp == 10:
+    elif exp == 50:  # Flexible UNet
         experiment_flexible_unet(
             config,
-            n=100, b=64, lr=5e-4, loss=LOSS_DICE_BCE,
+            n=100,
+            b=128,
+            lr=5e-4,
+            loss=LOSS_DICE_BCE,
             encoders=[4, 2, 1], decoders=[1, 2, 4], thickness=16,
         )
-        config[DATALOADER][AUGMENTATION_LIST] = [AUGMENTATION_H_ROLL_WRAPPED, AUGMENTATION_FLIP]
+    else:
+        raise ValueError(f"Experiment {exp} not found")
     return config
